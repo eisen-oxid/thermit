c8c40413dd16fe5e78a6e732213e852b6ed81e8a
Tobias <me+github@kantusch.dev>
2021-02-27 12:03:36 +0100

    Add basic http server with example routes
    Co-authored-by: frcroth <frcroth@gmail.com>

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# Server
+
+This is the thermit server.
+
+## Development
+
+The server can be build with `cargo build`.
+
+To run the server in watch mode, first install `cargo-watch` using `cargo install cargo-watch`. Then run `cargo watch -x run`.
+Now with every change made in the code, the server will automatically recompile and continue running.
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn home(_request: HttpRequest) -> impl Responder {
+    println!("Home visited");
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/home2")]
+async fn home2(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(home)).service(home2))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
