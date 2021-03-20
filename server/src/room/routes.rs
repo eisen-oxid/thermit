a77fcc39277116550b3f1bfd1a3dd36c05dd3d49
frcroth <frcroth@gmail.com>
2021-03-20 12:13:55 +0100

    Add api for users in rooms

--- server/src/room/routes.rs
+++ server/src/room/routes.rs
@@ -2,6 +2,7 @@ use crate::errors::ServiceError;
 use crate::room::{Room, RoomData};
 use crate::Pool;
 use actix_web::{delete, get, post, put, web, HttpResponse};
+use serde::Deserialize;
 use serde_json::json;
 use uuid::Uuid;
 
@@ -12,6 +13,7 @@ pub async fn list(pool: web::Data<Pool>) -> Result<HttpResponse, ServiceError> {
     let rooms = web::block(move || Room::find_all(&conn))
         .await
         .map_err(ServiceError::from)?;
+
     if rooms.is_empty() {
         Err(ServiceError::NoContent)
     } else {
@@ -31,7 +33,7 @@ pub async fn find(
         .map_err(ServiceError::from)?;
 
     if let Some(room) = room {
-        Ok(get_json_response(&pool, room))
+        get_json_response(&pool, room)
     } else {
         Err(ServiceError::NotFound)
     }
@@ -48,7 +50,7 @@ async fn create(
         .await
         .map_err(ServiceError::from)?;
 
-    Ok(get_json_response(&pool, room))
+    get_json_response(&pool, room)
 }
 
 #[put("/rooms/{id}")]
@@ -61,7 +63,8 @@ pub async fn update(
     let room = web::block(move || Room::update(&conn, id.into_inner(), room_data.into_inner()))
         .await
         .map_err(ServiceError::from)?;
-    Ok(get_json_response(&pool, room))
+
+    get_json_response(&pool, room)
 }
 
 #[delete("/rooms/{id}")]
@@ -82,11 +85,69 @@ pub async fn delete(
     }
 }
 
-fn get_json_response(pool: &web::Data<Pool>, room: Room) -> HttpResponse {
+#[derive(Deserialize, Debug)]
+pub struct RoomUserData {
+    id: Uuid,
+}
+
+#[post("/rooms/{id}/users")]
+pub async fn add_user(
+    pool: web::Data<Pool>,
+    room_id: web::Path<Uuid>,
+    user_data: web::Json<RoomUserData>,
+) -> Result<HttpResponse, ServiceError> {
     let conn = pool.get().expect("couldn't get db connection from pool");
 
-    let room_user_ids: Vec<Uuid> = Room::get_room_users(&conn, &room)
-        .unwrap()
+    Room::add_users(&conn, room_id.into_inner(), vec![user_data.into_inner().id])?;
+
+    Ok(HttpResponse::NoContent().finish())
+}
+
+#[get("/rooms/{id}/users")]
+pub async fn get_users(
+    pool: web::Data<Pool>,
+    room_id: web::Path<Uuid>,
+) -> Result<HttpResponse, ServiceError> {
+    let conn = pool.get().expect("couldn't get db connection from pool");
+
+    let possible_room = Room::find(&conn, room_id.into_inner())?;
+    let room = match possible_room {
+        None => return Err(ServiceError::NotFound),
+        Some(room) => room,
+    };
+
+    let room_user_ids: Vec<Uuid> = Room::get_room_users(&conn, &room)?
+        .into_iter()
+        .map(|room_user| room_user.user_id)
+        .collect();
+
+    Ok(HttpResponse::Ok().json(room_user_ids))
+}
+
+#[delete("/rooms/{room_id}/users/{user_id}")]
+pub async fn remove_user(
+    pool: web::Data<Pool>,
+    ids: web::Path<(Uuid, Uuid)>,
+) -> Result<HttpResponse, ServiceError> {
+    let conn = pool.get().expect("couldn't get db connection from pool");
+
+    let ids_content = ids.into_inner();
+    let room_id = ids_content.0;
+    let user_id = ids_content.1;
+
+    let count = Room::remove_users(&conn, room_id, vec![user_id])?;
+
+    if count == 0 {
+        Err(ServiceError::NotFound)
+    } else {
+        Ok(HttpResponse::NoContent().finish())
+    }
+}
+
+fn get_json_response(pool: &web::Data<Pool>, room: Room) -> Result<HttpResponse, ServiceError> {
+    let conn = pool.get().expect("couldn't get db connection from pool");
+
+    let room_user_ids: Vec<Uuid> = Room::get_room_users(&conn, &room)?
         .into_iter()
         .map(|room_user| room_user.user_id)
         .collect();
@@ -96,7 +157,7 @@ fn get_json_response(pool: &web::Data<Pool>, room: Room) -> HttpResponse {
         "name": room.name,
         "users": room_user_ids
     });
-    HttpResponse::Ok().json(room_json)
+    Ok(HttpResponse::Ok().json(room_json))
 }
 
 pub fn init_routes(config: &mut web::ServiceConfig) {
@@ -105,4 +166,8 @@ pub fn init_routes(config: &mut web::ServiceConfig) {
     config.service(create);
     config.service(update);
     config.service(delete);
+
+    config.service(add_user);
+    config.service(get_users);
+    config.service(remove_user);
 }
