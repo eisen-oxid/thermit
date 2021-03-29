2ee27429884ef4bf681d5b33c82a429a4993272b
frcroth <frcroth@gmail.com>
2021-03-29 19:12:48 +0200

    Remove rooms_users row when deleting user or room

--- server/migrations/2021-03-07-173414_create_rooms/up.sql
+++ server/migrations/2021-03-07-173414_create_rooms/up.sql
@@ -12,6 +12,6 @@ CREATE TABLE "rooms_users"
     room_id UUID,
     status VARCHAR,
     PRIMARY KEY (user_id, room_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (room_id) REFERENCES rooms(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE
 );
