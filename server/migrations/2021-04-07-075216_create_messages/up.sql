321c01dd15d54b2941e7b1c73f67828fcde14923
frcroth <frcroth@gmail.com>
2021-04-07 10:34:02 +0200

    Add updated and created to messages

--- server/migrations/2021-04-07-075216_create_messages/up.sql
+++ server/migrations/2021-04-07-075216_create_messages/up.sql
@@ -11,5 +11,12 @@ CREATE TABLE "messages"
     FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE,
     FOREIGN KEY (author) REFERENCES users(id) ON DELETE CASCADE,
     content VARCHAR NOT NULL,
-    message_encryption message_encryption_type NOT NULL
+    message_encryption message_encryption_type NOT NULL,
+    created timestamp NOT NULL default current_timestamp,
+    updated timestamp NOT NULL default current_timestamp
 );
+
+CREATE TRIGGER message_update_date_trigger
+    BEFORE UPDATE ON messages
+    FOR EACH ROW
+EXECUTE PROCEDURE last_upd_trig();
