c984d89b397791ad640a83082285f70789de8c7f
frcroth <frcroth@gmail.com>
2021-05-01 10:49:45 +0200

    Add generic crud for messages

--- server/src/message/model.rs
+++ server/src/message/model.rs
@@ -1,10 +1,10 @@
 use diesel::prelude::*;
+use diesel::result::Error as DieselError;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::schema::messages;
 use chrono::NaiveDateTime;
-use crate::message::MessageError::MessageNotFound;
 
 #[derive(Serialize, Deserialize, Queryable, Insertable, PartialEq, Debug)]
 #[table_name = "messages"]
@@ -27,23 +27,57 @@ pub struct MessageData {
 }
 
 pub enum MessageError {
-    MessageNotFound
+    MessageNotFound,
+    DatabaseError,
+    GenericError,
 }
 
 impl Message {
     pub fn find(conn: &PgConnection, message_id: Uuid) -> Result<Option<Message>, MessageError> {
-        Err(MessageNotFound)
+        use crate::schema::messages::dsl::*;
+
+        Ok(messages
+            .find(message_id)
+            .get_result::<Message>(conn)
+            .optional()?)
     }
 
     pub fn create(message_data: MessageData, conn: &PgConnection) -> Result<Message, MessageError> {
-        Err(MessageNotFound)
+        use crate::schema::messages::dsl::*;
+
+        let new_message: Message = diesel::insert_into(messages)
+            .values(&message_data)
+            .get_result(conn)?;
+        Ok(new_message)
     }
 
-    pub fn update(message_id: Uuid, message_data: MessageData,conn: &PgConnection,) -> Result<Message, MessageError> {
-        Err(MessageNotFound)
+    pub fn update(
+        message_id: Uuid,
+        message_data: MessageData,
+        conn: &PgConnection,
+    ) -> Result<Message, MessageError> {
+        use crate::schema::messages::dsl::*;
+
+        let message: Message = diesel::update(messages.find(message_id))
+            .set(message_data)
+            .get_result(conn)?;
+        Ok(message)
     }
 
     pub fn destroy(message_id: Uuid, conn: &PgConnection) -> Result<usize, MessageError> {
-        Err(MessageNotFound)
+        use crate::schema::messages::dsl::*;
+
+        let count = diesel::delete(messages.find(message_id)).execute(conn)?;
+        Ok(count)
+    }
+}
+
+impl From<DieselError> for MessageError {
+    fn from(error: DieselError) -> MessageError {
+        match error {
+            DieselError::DatabaseError(_, _) => MessageError::DatabaseError,
+            DieselError::NotFound => MessageError::MessageNotFound,
+            _ => MessageError::GenericError,
+        }
     }
 }
